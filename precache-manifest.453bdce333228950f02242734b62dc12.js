b10ba51e3a06e2efa9cee178dbb6e4f4ea7fac15
Pekka Laine <pekkaarvolaine@gmail.com>
2019-12-29 22:11:42 +0200

    Updates

---
+++ precache-manifest.453bdce333228950f02242734b62dc12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b65559a01cbcf5c92f377153455462b",
+    "url": "/counterapp/index.html"
+  },
+  {
+    "revision": "8f500f588fbd0d5f221d",
+    "url": "/counterapp/static/css/main.4b188e4e.chunk.css"
+  },
+  {
+    "revision": "97393241301299f888e5",
+    "url": "/counterapp/static/js/2.cfc04879.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/counterapp/static/js/2.cfc04879.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f500f588fbd0d5f221d",
+    "url": "/counterapp/static/js/main.c0676870.chunk.js"
+  },
+  {
+    "revision": "0aa4d6c1418c06abba3b",
+    "url": "/counterapp/static/js/runtime-main.980a9e01.js"
+  }
+]);
